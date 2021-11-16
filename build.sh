d4570a3d2113d0b3b988bea6f7dc1404ded71963
William Jackson <william@subtlecoolness.com>
2021-11-16 10:41:57 -0600

    Add build dependencies to build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-# apk add --no-cache gcc musl-dev
+apk add --no-cache file g++ gcc libffi-dev make musl-dev
 
 # build
 pip wheel --no-deps --requirement requirements.txt
